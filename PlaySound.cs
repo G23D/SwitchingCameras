981d66df1fe42fd8887c365339e46a61b9fb9bef
Kyle "G <kyle@electricrobots.com>
2015-06-18 18:02:27 -0400

    added play sound script

---
+++ PlaySound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+	public AudioClip impact;
+	AudioSource audio;
+	
+	void Update() {
+		audio = GetComponent<AudioSource> ();
+	
+		if (Input.GetKeyDown ("o")) {
+			audio.PlayOneShot (impact, 0.7F);
+		}
+	}
+}

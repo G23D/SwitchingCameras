a1d3d5c21bf350f45629533a52b3a47f6cacbe03
Kyle "G <kyle@electricrobots.com>
2015-06-18 13:50:16 -0400

    license and instructions

--- SwitchCameras.cs
+++ SwitchCameras.cs
@@ -1,4 +1,19 @@
-﻿//Credit to "TheArtist" for this script http://answers.unity3d.com/users/515523/theartist.html
+﻿//Credit to "TheArtist" for this script http://answers.unity3d.com/users/515523/theartist.html & http://g23d.com for instructions.
+/*
+ * Put this script in a logical folder in your Unity3D project folders.
+ * In the Unity3D editor go to the upper left menu and under "Game Object" select "Create Empty".  
+ * Rename it in the Heirarchy tab as "Cameras".  Drag this script from the "Project" tab to
+ * the "Cameras" empty game object in the Heirarchy tab.  Next go to the upper left menu and
+ * under "Game Object" click "Camera". This adds and additional camera to your scene.  Rename this camera
+ * "Camera2". Drag this new camera onto the "Cameras" 
+ * empty game object in the heirarchy tab making it a child of that object for organizational purposes. Highlight 
+ * this camera in the Heirarchy tab then in the Inspector tab select the "MainCamera" tag.  Now highlight the 
+ * "Cameras" empty game object in the Heirarchy tab and in the inspector change the "Size" field to 2. Click the small
+ * circle to the right of the first field and select the "MainCamera" then the second fields circle click and select
+ * "Camera2".  Finally position this camera where you want and click the "Play" button.  Hit your "C" key to switch
+ * views.  Add more cameras as needed.
+
+ */
 using UnityEngine;
 using System.Collections;
 

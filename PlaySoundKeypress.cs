1b172b88d0c081d365df98eecb166e71c87c817d
Kyle "G <kyle@electricrobots.com>
2015-06-18 18:09:33 -0400

    added play sound keypress

---
+++ PlaySoundKeypress.cs
@@ -0,0 +1,22 @@
+﻿/*
+Apply this script to a game object then in the inspector choose an audio file via the 
+"Impact field.  Uncheck "Play On Awake" then hit the "o" key to start playing the audio.
+*/
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlaySoundKeypress : MonoBehaviour {
+
+	public AudioClip impact;
+	AudioSource audio;
+	
+	void Update() {
+		audio = GetComponent<AudioSource> ();
+//edit the "o" on Line 16 to another key such as "l" etc.	
+		if (Input.GetKeyDown ("o")) {
+			audio.PlayOneShot (impact, 0.7F);
+		}
+	}
+}
